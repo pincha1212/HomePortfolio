0851122afa5402b52683a02201b2caee00b88e48
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-05-14 00:55:25 -0300

    perzonalizacion moviles, completo

--- fdb/src/app/components/menu-moviles/menu-moviles.component.ts
+++ fdb/src/app/components/menu-moviles/menu-moviles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-menu-moviles',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-moviles.component.css']
 })
 export class MenuMovilesComponent {
+  constructor(private router: Router) {}
 
+  goHome = () => {
+    this.router.navigateByUrl('');
+  };
+  btnClick = () => {
+    this.router.navigateByUrl('/sobre-mi');
+  };
+  btnClick2 = () => {
+    this.router.navigateByUrl('/habilidades');
+  };
+  btnClick3 = () => {
+    this.router.navigateByUrl('/proyectos');
+  };
+
+  btnClick4 = () => {
+    this.router.navigateByUrl('/menu-moviles');
+  }
 }
