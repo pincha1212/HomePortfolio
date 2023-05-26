7c0e301617822675fec67182f5d0ad16f448f97d
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-05-25 22:28:10 -0300

    formulario desde cero

--- fdb/src/app/components/contactame/contactame.module.ts
+++ fdb/src/app/components/contactame/contactame.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactameRoutingModule } from './contactame-routing.module';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FormComponent
+  ],
   imports: [
     CommonModule,
     ContactameRoutingModule
